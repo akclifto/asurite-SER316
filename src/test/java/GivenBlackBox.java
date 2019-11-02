7e312546ab354ef763e7e7dbb0a2b1ce92cea544
Adam Clifton <akclifto@asu.edu>
2019-11-02 14:29:12 -0700

    adding cls folder and first test of Black box

--- src/test/java/GivenBlackBox.java
+++ src/test/java/GivenBlackBox.java
@@ -12,9 +12,7 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
-import main.java.Course;
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(Parameterized.class)
 public class GivenBlackBox {
@@ -22,6 +20,7 @@ public class GivenBlackBox {
 
     @SuppressWarnings("unchecked")
     public GivenBlackBox(Object classUnderTest) {
+
         this.classUnderTest = (Class<Course>) classUnderTest;
     }
 
@@ -50,44 +49,87 @@ public class GivenBlackBox {
     Course happyDayGradeBoundary;
     HashMap<String, String> happyDayGradeBoundaryExpected = new HashMap<String, String>();
 
+    Course centerBoundaryPartition;
+    HashMap<String, String> centerBoundaryPartitionExpected = new HashMap<String, String>();
 
     @Before
     public void setUp() throws Exception {
 
         // One Student
-
-
         // all courses should be created like this
-
-
         // Course created with two Students having
         oneStudent = createCourse("SER316");
+//        oneStudent = createCourse("CSE101");
 
         // this would be the expected result after the method countOccurencesLetterGrades is called
         oneStudent.set_points("Hanna", 50);
         oneStudentExpected.put("Hanna", "A");
 
+
         // Happy Day Case Grade Boundaries
         // Four Students mix of grades
         happyDayGradeBoundary = createCourse("SER315");
-        happyDayGradeBoundary.set_points("100"  , 100);  //A
+        happyDayGradeBoundary.set_points("100"  , 100); //A
         happyDayGradeBoundary.set_points(">89"  , 90);  //A
         happyDayGradeBoundary.set_points(">79"  , 89);  //B
         happyDayGradeBoundary.set_points(">59"  , 79);  //C
         happyDayGradeBoundary.set_points(">35"  , 59);  //D
-        happyDayGradeBoundary.set_points(">=35" , 35);  //F
-
+        happyDayGradeBoundary.set_points("<=35" , 35);  //F
+        //expected result
         happyDayGradeBoundaryExpected.put("100"  , "A");
         happyDayGradeBoundaryExpected.put(">89"  , "A");
         happyDayGradeBoundaryExpected.put(">79"  , "B");
         happyDayGradeBoundaryExpected.put(">59"  , "C");
         happyDayGradeBoundaryExpected.put(">35"  , "D");
-        happyDayGradeBoundaryExpected.put(">=35" , "F");
+        happyDayGradeBoundaryExpected.put("<=35" , "F");
+
+
+        //Center checks 1-to-1, no curve will occur due to someone getting 100
+        centerBoundaryPartition = createCourse("SER216");
+        centerBoundaryPartition.set_points(">100", 110); //A
+        centerBoundaryPartition.set_points("=95", 95); //A
+        centerBoundaryPartition.set_points("=85", 85); //B
+        centerBoundaryPartition.set_points("=75", 75); //C
+        centerBoundaryPartition.set_points("=65", 65); //C
+        centerBoundaryPartition.set_points("=55", 55); //D
+        centerBoundaryPartition.set_points("=45", 45); //D
+        centerBoundaryPartition.set_points("=35", 35); //F
+        centerBoundaryPartition.set_points("=25", 25); //F
+        centerBoundaryPartition.set_points("=15", 15); //F
+        centerBoundaryPartition.set_points("=05", 05); //F
+
+        //Center Boundary expectation
+        centerBoundaryPartitionExpected.put(">100", "A");
+        centerBoundaryPartitionExpected.put("=95", "A");
+        centerBoundaryPartitionExpected.put("=85", "B");
+        centerBoundaryPartitionExpected.put("=75", "C");
+        centerBoundaryPartitionExpected.put("=65", "C");
+        centerBoundaryPartitionExpected.put("=55", "D");
+        centerBoundaryPartitionExpected.put("=45", "D");
+        centerBoundaryPartitionExpected.put("=35", "F");
+        centerBoundaryPartitionExpected.put("=25", "F");
+        centerBoundaryPartitionExpected.put("=15", "F");
+        centerBoundaryPartitionExpected.put("=05", "F");
+
     }
 
     @After
     public void tearDown() throws Exception {
     }
+
+    @Test
+    public void three_pt_boundary_check(){
+
+    }
+
+    @Test
+    public void center_check_no_curve(){
+        Map<String, String> cen = centerBoundaryPartition.curveLetterGrades();
+        for(Map.Entry<String, String> e : cen.entrySet())
+            System.out.println(e.getKey() + " " + e.getValue());
+        assertTrue(cen.equals(centerBoundaryPartitionExpected));
+    }
+
 
     // sample test
     @Test
@@ -106,5 +148,7 @@ public class GivenBlackBox {
             System.out.println(e.getKey() + " " + e.getValue());
         assertTrue(ans.equals(happyDayGradeBoundaryExpected));
     }
+
+
 
 }
