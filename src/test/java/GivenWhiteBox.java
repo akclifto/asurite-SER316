725e7827e8b97d2c93a80b147982573eb6d10a65
Adam Clifton <akclifto@asu.edu>
2019-11-09 17:43:39 -0700

    testing for addStudent and dropStudent complete

--- src/test/java/GivenWhiteBox.java
+++ src/test/java/GivenWhiteBox.java
@@ -1,7 +1,11 @@
 import main.java.Course;
+import main.java.Major;
+import main.java.Student;
 import org.junit.Before;
 import org.junit.Test;
+
 import java.util.HashMap;
+
 import static org.junit.Assert.assertTrue;
 
 public class GivenWhiteBox {
@@ -25,4 +29,18 @@ public class GivenWhiteBox {
         assertTrue(ans.get("D") == 0);
         assertTrue(ans.get("F") == 0);
     }
+
+    @Test
+    public void addDropStudentTest(){
+        //add
+        Student s1 = new Student("2144", Major.CS);
+        Course c1 = new Course ("CSE101");
+        c1.addStudent(s1);
+        assertTrue(c1.getStudents().size() == 2);
+        assertTrue(!c1.addStudent(s1));
+        //drop
+        c1.dropStudent("2144");
+        assertTrue(c1.getStudents().get(0) == s1);
+    }
+
 }
